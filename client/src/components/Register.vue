77260f285f5a367a8759ec0feda52c3fe26c6695
iustin <iustin.nitza@gmail.com>
2018-04-09 17:24:22 +0200

    add register endpoint and root

---
+++ client/src/components/Register.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input v-model="email" type="email" name="email" placeholder="Email"/>
+    <input v-model="password" type="password" name="password" placeholder="Password"/>
+    <button type="submit" @click="register()">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  name: 'Register',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data);
+    }
+  }
+
+}
+</script>
